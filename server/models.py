a96aa60ebc9f640e294328ace2682ee472cab7bb
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-06 10:16:12 -0400

    completed

--- server/models.py
+++ server/models.py
@@ -12,6 +12,20 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def valid_name(self, key, name):
+        if not len(name):
+            raise ValueError('Must supply a name')
+        is_duplicate = Author.query.filter_by(name=name).first()
+        if is_duplicate:
+            raise ValueError('Author name must be unique')
+        return name
+
+    @validates('phone_number')
+    def valid_phone(self, key, number):
+        if len(str(number)) != 10 or not int(number):
+            raise ValueError('Phone number must be exactly 10 digits')
+        return number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,7 +42,35 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('title')
+    def valid_title(self, key, title):
+        required_title_words = ["Won't Believe", "Secret", "Top", "Guess"]
+        title_is_valid = False
+        for word in required_title_words:
+            if word in title:
+                title_is_valid = True
+        if not title_is_valid:
+            raise ValueError('Title must contain clickbait keywords!')
+        return title
+    
+    @validates('content')
+    def valid_post_length(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Post length must be greater than 250 characters.')
+        return content
 
+    @validates('summary')
+    def valid_summary_length(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('Summary must be less than 250 characters')
+        return summary
+
+    @validates('category')
+    def valid_category(self, key, cat):
+        valid_cat_list = ['Fiction', 'Non-Fiction']
+        if not cat in valid_cat_list:
+            raise ValueError('Category is not valid.')
+        return cat
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
